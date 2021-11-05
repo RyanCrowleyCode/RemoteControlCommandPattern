4d90b78345e4d2aefc1f1cf1a0e9fe37cd11bcc3
Ryan Crowley <ryan.crowley@daveramsey.com>
2021-11-05 17:07:09 -0500

    Added GarageDoorOpenCommand

---
+++ src/GarageDoor.java
@@ -0,0 +1,23 @@
+public class GarageDoor {
+    public GarageDoor() {}
+
+    public void up() {
+        System.out.println("Garage Door is Open");
+    }
+
+    public void down() {
+        System.out.println("Garage Door is Closed");
+    }
+
+    public void stop() {
+        System.out.println("Garage Door has Stopped");
+    }
+
+    public void lightOn() {
+        System.out.println("Garage Door light is On");
+    }
+
+    public void lightOff() {
+        System.out.println("Garage Door light is Off");
+    }
+}
